f2d7f6ec4716952cb93258b985d1402ba23b1c09
dulanakalya <dulanakalya@gmail.com>
2025-07-29 21:28:59 +0530

    data validation

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from src.machinelearningproject import logger
-
+from src.machinelearningproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.machinelearningproject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 logger.info("Welcome to our custom")
+
+STAGE_NAME="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
