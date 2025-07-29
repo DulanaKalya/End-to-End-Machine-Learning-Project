309863d3a5a2521b8139c8c2225f563784a908a5
dulanakalya <dulanakalya@gmail.com>
2025-07-29 22:42:44 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.machinelearningproject import logger
 from src.machinelearningproject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.machinelearningproject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.machinelearningproject.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 logger.info("Welcome to our custom")
 
 STAGE_NAME="Data Ingestion Stage"
@@ -19,6 +20,16 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.initiate_data_validation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.initiate_data_transformation()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
