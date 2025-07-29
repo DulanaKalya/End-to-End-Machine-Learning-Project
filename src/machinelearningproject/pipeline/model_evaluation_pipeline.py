c7c03679f78ca9ccc32d09b673395af49ea2d497
dulanakalya <dulanakalya@gmail.com>
2025-07-30 00:34:40 +0530

    model evaluation

---
+++ src/machinelearningproject/pipeline/model_evaluation_pipeline.py
@@ -0,0 +1,17 @@
+from src.machinelearningproject.config.configuration import ConfigurationManager
+from src.machinelearningproject.components.model_evaluation import ModelEvaluation
+from src.machinelearningproject import logger
+
+STAGE_NAME = "Model evaluation stage"
+
+
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.log_into_mlflow()
